8ef430b2b2798c991cb2a2f8c706092ca4eb3377
patricia pucheta <ptrcpucheta@gmail.com>
2022-04-29 17:12:13 -0300

    Correccion ModificarCliente

--- Shopping_Buy_All/Shopping_Buy_All/Client_Modify.cs
+++ Shopping_Buy_All/Shopping_Buy_All/Client_Modify.cs
@@ -29,7 +29,7 @@ namespace Shopping_Buy_All
             }
             else
             {
-            Cliente c = Buscar_Cliente_Documento(comboBoxDocType.SelectedIndex, textNumberDoc.Text);
+            Cliente c = Buscar_Cliente_Documento((int)comboBoxDocType.SelectedValue, textNumberDoc.Text);
             Cargar_Campos(c);
             SearchPanel.Visible=false;
             btnSearchClient.Visible=false;
@@ -187,7 +187,7 @@ namespace Shopping_Buy_All
             try
             {
                 SqlCommand cmd = new SqlCommand();
-                string consulta = "SELECT * FROM Clientes where TipoDocumendo like @tipoDocumento AND NroDocumento like @nrodocumento";
+                string consulta = "SELECT * FROM Clientes where TipoDocumento like @tipoDocumento AND NroDocumento like @nrodocumento";
 
                 cmd.Parameters.Clear();
                 cmd.Parameters.AddWithValue("@nroDocumento", NroDocumento);
