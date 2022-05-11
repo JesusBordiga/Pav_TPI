14c6d4de982cf4ed910637783888e0e252f06576
FrancoRav <raviolofranco@gmail.com>
2022-05-11 00:41:23 -0300

    Modificacion Marca Vehiculo

---
+++ Shopping_Buy_All/Shopping_Buy_All/ABMS/ABM MarcaVehiculo/MarcaVehiculo_Modify.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Shopping_Buy_All
+{
+    partial class MarcaVehiculo_Modify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MarcaVehiculo_Modify));
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.labelMarca = new System.Windows.Forms.Label();
+            this.textMarcaOld = new System.Windows.Forms.TextBox();
+            this.tablaMarcas = new System.Windows.Forms.DataGridView();
+            this.textMarcaNew = new System.Windows.Forms.TextBox();
+            this.labelMod = new System.Windows.Forms.Label();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Borrado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaMarcas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(189, 316);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Limpiar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnModify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModify.ForeColor = System.Drawing.Color.White;
+            this.btnModify.Location = new System.Drawing.Point(29, 316);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 23);
+            this.btnModify.TabIndex = 3;
+            this.btnModify.Text = "Modificar";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Visible = false;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(302, 16);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(151, 22);
+            this.label15.TabIndex = 68;
+            this.label15.Text = "Lista de Marcas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(50, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 22);
+            this.label1.TabIndex = 61;
+            this.label1.Text = "Modificar Marca";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(109, 316);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // labelMarca
+            // 
+            this.labelMarca.AutoSize = true;
+            this.labelMarca.BackColor = System.Drawing.Color.Transparent;
+            this.labelMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMarca.ForeColor = System.Drawing.Color.White;
+            this.labelMarca.Location = new System.Drawing.Point(101, 125);
+            this.labelMarca.Name = "labelMarca";
+            this.labelMarca.Size = new System.Drawing.Size(52, 17);
+            this.labelMarca.TabIndex = 73;
+            this.labelMarca.Text = "Marca";
+            // 
+            // textMarcaOld
+            // 
+            this.textMarcaOld.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textMarcaOld.ForeColor = System.Drawing.Color.White;
+            this.textMarcaOld.Location = new System.Drawing.Point(72, 145);
+            this.textMarcaOld.Name = "textMarcaOld";
+            this.textMarcaOld.Size = new System.Drawing.Size(110, 20);
+            this.textMarcaOld.TabIndex = 1;
+            // 
+            // tablaMarcas
+            // 
+            this.tablaMarcas.AllowUserToAddRows = false;
+            this.tablaMarcas.AllowUserToDeleteRows = false;
+            this.tablaMarcas.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.tablaMarcas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tablaMarcas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.tablaMarcas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaMarcas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Descripcion,
+            this.Borrado});
+            this.tablaMarcas.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tablaMarcas.Location = new System.Drawing.Point(279, 44);
+            this.tablaMarcas.Name = "tablaMarcas";
+            this.tablaMarcas.ReadOnly = true;
+            this.tablaMarcas.RowHeadersWidth = 20;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            this.tablaMarcas.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.tablaMarcas.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tablaMarcas.Size = new System.Drawing.Size(205, 310);
+            this.tablaMarcas.TabIndex = 91;
+            this.tablaMarcas.TabStop = false;
+            this.tablaMarcas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tablaRubros_CellContentClick);
+            // 
+            // textMarcaNew
+            // 
+            this.textMarcaNew.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textMarcaNew.ForeColor = System.Drawing.Color.White;
+            this.textMarcaNew.Location = new System.Drawing.Point(72, 145);
+            this.textMarcaNew.Name = "textMarcaNew";
+            this.textMarcaNew.Size = new System.Drawing.Size(110, 20);
+            this.textMarcaNew.TabIndex = 2;
+            this.textMarcaNew.Visible = false;
+            // 
+            // labelMod
+            // 
+            this.labelMod.AutoSize = true;
+            this.labelMod.BackColor = System.Drawing.Color.Transparent;
+            this.labelMod.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMod.ForeColor = System.Drawing.Color.White;
+            this.labelMod.Location = new System.Drawing.Point(71, 125);
+            this.labelMod.Name = "labelMod";
+            this.labelMod.Size = new System.Drawing.Size(113, 17);
+            this.labelMod.TabIndex = 95;
+            this.labelMod.Text = "Nuevo nombre";
+            this.labelMod.Visible = false;
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 35;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.DataPropertyName = "Descripcion";
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.ReadOnly = true;
+            this.Descripcion.Width = 150;
+            // 
+            // Borrado
+            // 
+            this.Borrado.DataPropertyName = "Borrado";
+            this.Borrado.HeaderText = "Borrado";
+            this.Borrado.Name = "Borrado";
+            this.Borrado.ReadOnly = true;
+            this.Borrado.Visible = false;
+            // 
+            // MarcaVehiculo_Modify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImage = global::Shopping_Buy_All.Properties.Resources.montanas_minimalista_arte_low_poly_2560x1440_xtrafondos_com;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(499, 364);
+            this.Controls.Add(this.tablaMarcas);
+            this.Controls.Add(this.textMarcaOld);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.labelMarca);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textMarcaNew);
+            this.Controls.Add(this.labelMod);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MarcaVehiculo_Modify";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modificar Marca Vehiculo";
+            ((System.ComponentModel.ISupportInitialize)(this.tablaMarcas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label labelMarca;
+        private System.Windows.Forms.TextBox textMarcaOld;
+        private System.Windows.Forms.DataGridView tablaMarcas;
+        private System.Windows.Forms.TextBox textMarcaNew;
+        private System.Windows.Forms.Label labelMod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Borrado;
+    }
+}
---
+++ Shopping_Buy_All/Shopping_Buy_All/ABMS/ABM MarcaVehiculo/MarcaVehiculo_Modify.cs
@@ -0,0 +1,256 @@
+﻿using Shopping_Buy_All.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping_Buy_All
+{
+    public partial class MarcaVehiculo_Modify : Form
+    {
+        public MarcaVehiculo_Modify()
+        {
+            InitializeComponent();
+            CargarTablaMarcas();
+        }
+
+        private void Clean()
+        {
+            textMarcaOld.Text = "";
+            textMarcaNew.Text = "";
+            textMarcaOld.Visible = true;
+            textMarcaNew.Visible = false;
+            btnModify.Visible = false;
+            btnSearch.Visible = true;
+            labelMarca.Visible = true;
+            labelMod.Visible = false;
+            textMarcaOld.Focus();
+        }
+
+        private void Cargar_Campos(string marca)
+        {
+            //Cargar Nombre Original
+            textMarcaOld.Text = marca;
+            //Cargar Nombre Nuevo
+            textMarcaNew.Text = marca;
+            btnModify.Visible = true;
+            textMarcaOld.Visible = false;
+            textMarcaNew.Visible = true;
+            btnSearch.Visible = false;
+            labelMarca.Visible = false;
+            labelMod.Visible = true;
+            textMarcaNew.Focus();
+        }
+
+        private void CargarTablaMarcas()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBaseDatos"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                SqlCommand comand = new SqlCommand();
+                string consulta = "Select * FROM Marcas WHERE Borrado like 0";
+
+                comand.Parameters.Clear();
+                comand.CommandType = CommandType.Text;
+                comand.CommandText = consulta;
+
+                cn.Open();
+                comand.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(comand);
+                da.Fill(tabla);
+                tablaMarcas.DataSource = tabla;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error en la base de datos.", "ERROR");
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private bool ExisteMarca(string marca)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBaseDatos"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            bool result = false;
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT * FROM Marcas WHERE Descripcion = @marca AND Borrado like 0";
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@marca", marca);
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                SqlDataReader DataReader = cmd.ExecuteReader();
+                if (DataReader != null && DataReader.Read())
+                {
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error en la base de datos.", "ERROR");
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return result;
+        }
+
+        private string ObtenerDatos()
+        {
+            string r = textMarcaNew.Text.Trim();
+            return r;
+        }
+
+        private bool ModificarRubro(string newM, string oldM)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBaseDatos"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            bool resultado = false;
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "UPDATE Marcas SET Descripcion = @newNombre WHERE Descripcion = @oldNombre";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@newNombre", newM);
+                cmd.Parameters.AddWithValue("@oldNombre", oldM);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                resultado = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo modificar la Marca.\nError en la base de datos.", "ERROR");
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return resultado;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (textMarcaOld.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("Error, Completar campos!!");
+            }
+            else
+            {
+                string rubro = textMarcaOld.Text.Trim();
+                bool existe = ExisteMarca(rubro);
+                if (existe)
+                {
+                    Cargar_Campos(rubro);
+                }
+                else
+                {
+                    MessageBox.Show("La Marca buscada no existe.");
+                    Clean();
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clean();
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos())
+            {
+                string newR = ObtenerDatos();
+                if (!ExisteMarca(newR))
+                {
+                    string oldR = textMarcaOld.Text.Trim().ToLower();
+                    bool resultado = ModificarRubro(newR, oldR);
+                    if (resultado)
+                    {
+                        MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+                        String mensajeCarga = (
+                              " | Marca: " + newR + " |" + "\n");
+
+                        string titulo = "Información de Marca";
+
+                        DialogResult result = MessageBox.Show(mensajeCarga, titulo, buttons);
+
+                        if (result == DialogResult.OK)
+                        {
+                            MessageBox.Show("Marca modificada con éxito!");
+                            Clean();
+                            CargarTablaMarcas();
+                        }
+                        else
+                        {
+                            textMarcaOld.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al modificar la Marca! \n" +
+                                "Complete los campos por favor!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error al modificar la Marca! \n" +
+                            "Ya existe un rubro con ese nombre!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error al modificar la Marca! \n" +
+                            "Complete los campos por favor!");
+            }
+        }
+
+        private bool ValidarCampos()
+        {
+            // Validar que no esté vacío o no tenga más de 50 caracteres
+            if (textMarcaNew.Text.Trim() == "" || textMarcaNew.Text.Trim().Length > 50)
+            {
+                return false;
+            }
+            return true;
+        }
+        private void tablaRubros_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int indice = e.RowIndex;
+                DataGridViewRow filaSeleccionada = tablaMarcas.Rows[indice];
+                string rubro = filaSeleccionada.Cells["Descripcion"].Value.ToString();
+                Cargar_Campos(rubro);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Seleccione una casilla dentro de la tabla", "ERROR");
+            }
+        }
+    }
+}
