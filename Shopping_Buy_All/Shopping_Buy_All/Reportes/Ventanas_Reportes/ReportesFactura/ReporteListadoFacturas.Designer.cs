9275d0b0f2431cb73db51218649925d50454f5e9
JesusBordiga <jesus_bordiga@hotmail.com.ar>
2022-06-10 16:34:43 -0300

    actualizacion de reportes factura

--- Shopping_Buy_All/Shopping_Buy_All/Reportes/Ventanas_Reportes/ReportesFactura/ReporteListadoFacturas.Designer.cs
+++ Shopping_Buy_All/Shopping_Buy_All/Reportes/Ventanas_Reportes/ReportesFactura/ReporteListadoFacturas.Designer.cs
@@ -31,16 +31,20 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReporteListadoFacturas));
             this.reportViewerFacturas = new Microsoft.Reporting.WinForms.ReportViewer();
             this.LabelFactura = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbOrdenar = new System.Windows.Forms.ComboBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // reportViewerFacturas
             // 
             this.reportViewerFacturas.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.reportViewerFacturas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.reportViewerFacturas.LocalReport.ReportEmbeddedResource = "Shopping_Buy_All.Reportes.Ventanas_Reportes.ReportesFactura.ListadoFactura.rdlc";
-            this.reportViewerFacturas.Location = new System.Drawing.Point(12, 41);
+            this.reportViewerFacturas.Location = new System.Drawing.Point(12, 78);
             this.reportViewerFacturas.Name = "reportViewerFacturas";
             this.reportViewerFacturas.ServerReport.BearerToken = null;
-            this.reportViewerFacturas.Size = new System.Drawing.Size(751, 497);
+            this.reportViewerFacturas.Size = new System.Drawing.Size(751, 460);
             this.reportViewerFacturas.TabIndex = 0;
             this.reportViewerFacturas.Load += new System.EventHandler(this.reportViewerFacturas_Load);
             // 
@@ -56,11 +60,51 @@
             this.LabelFactura.TabIndex = 8;
             this.LabelFactura.Text = "Listado de facturas";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(226, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Odernar por";
+            // 
+            // cmbOrdenar
+            // 
+            this.cmbOrdenar.BackColor = System.Drawing.Color.White;
+            this.cmbOrdenar.ForeColor = System.Drawing.Color.Black;
+            this.cmbOrdenar.FormattingEnabled = true;
+            this.cmbOrdenar.Location = new System.Drawing.Point(334, 50);
+            this.cmbOrdenar.Name = "cmbOrdenar";
+            this.cmbOrdenar.Size = new System.Drawing.Size(119, 21);
+            this.cmbOrdenar.TabIndex = 13;
+            this.cmbOrdenar.SelectedIndexChanged += new System.EventHandler(this.cmbOrdenar_SelectedIndexChanged);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnOrdenar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOrdenar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrdenar.ForeColor = System.Drawing.Color.White;
+            this.btnOrdenar.Location = new System.Drawing.Point(459, 49);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 14;
+            this.btnOrdenar.Text = "Odenar";
+            this.btnOrdenar.UseVisualStyleBackColor = false;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
             // ReporteListadoFacturas
             // 
             this.BackgroundImage = global::Shopping_Buy_All.Properties.Resources.montanas_minimalista_arte_low_poly_2560x1440_xtrafondos_com;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(773, 548);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.cmbOrdenar);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.LabelFactura);
             this.Controls.Add(this.reportViewerFacturas);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -80,5 +124,8 @@
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewerFacturas;
         private System.Windows.Forms.Label LabelFactura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbOrdenar;
+        private System.Windows.Forms.Button btnOrdenar;
     }
 }
--- Shopping_Buy_All/Shopping_Buy_All/Reportes/Ventanas_Reportes/ReportesFactura/ReporteListadoFacturas.cs
+++ Shopping_Buy_All/Shopping_Buy_All/Reportes/Ventanas_Reportes/ReportesFactura/ReporteListadoFacturas.cs
@@ -51,13 +51,69 @@ namespace Shopping_Buy_All
                 cn.Close();
             }
         }
+        
         private void ReporteListadoFacturas_Load(object sender, EventArgs e)
         {
             this.reportViewerFacturas.RefreshReport();
+            CargarElementosFactura();
+            btnOrdenar.Enabled = false;
         }
-
-        private  void reportViewerFacturas_Load(object sender, EventArgs e)
+        public static object ObtenerElementoFactura()
         {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBaseDatos"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                SqlCommand comand = new SqlCommand();
+                string consulta = "select * from ElementosFactura where Borrado = 0";
+
+                comand.Parameters.Clear();
+                comand.CommandType = CommandType.Text;
+                comand.CommandText = consulta;
+
+                cn.Open();
+                comand.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(comand);
+                da.Fill(tabla);
+
+                return tabla;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("No se pudo Obtener elementos de factura.\nError en la base de datos.", "ERROR");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo Obtener  elementos de factura.\nError en la base de datos.", "ERROR");
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        private void CargarElementosFactura()
+        {
+            try
+            {
+                cmbOrdenar.DataSource = ObtenerElementoFactura();
+                cmbOrdenar.DisplayMember = "Nombre";
+                cmbOrdenar.ValueMember = "Id";
+                cmbOrdenar.SelectedIndex = -1;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error, no se pudo Otener Datos de Tipos de Documentos");
+            }
+        }
+        private void reportViewerFacturas_Load(object sender, EventArgs e)
+            {
             DataTable TablaFacturas = new DataTable();
             TablaFacturas = ObtenerFacturas();
 
@@ -66,6 +122,21 @@ namespace Shopping_Buy_All
             reportViewerFacturas.LocalReport.DataSources.Clear();
             reportViewerFacturas.LocalReport.DataSources.Add(ds);
             reportViewerFacturas.LocalReport.Refresh();
+        }
+
+        private void cmbOrdenar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnOrdenar.Enabled = true;
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            if (!cmbOrdenar.SelectedIndex.Equals(-1))
+            {
+                int opcion = cmbOrdenar.SelectedIndex; 
+
+            }
+            
         }
     }
 }
