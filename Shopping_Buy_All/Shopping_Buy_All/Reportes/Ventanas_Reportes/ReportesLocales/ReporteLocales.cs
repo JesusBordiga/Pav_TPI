b9e5591ca2a286d8b36950e6d70c537e6f076e80
JulietaRodriguez08 <102389890+JulietaRodriguez08@users.noreply.github.com>
2022-06-18 19:35:03 -0300

    reportes

---
+++ Shopping_Buy_All/Shopping_Buy_All/Reportes/Ventanas_Reportes/ReportesLocales/ReporteLocales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping_Buy_All.Reportes.Ventanas_Reportes.ReportesLocales
+{
+    public partial class ReporteLocales : Form
+    {
+        public ReporteLocales()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteLocales_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
