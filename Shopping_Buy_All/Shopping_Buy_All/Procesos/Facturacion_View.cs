578a7c2823db2ac75e279199c4c07f80648892d3
Leangomez7 <leangomez7@hotmail.com>
2022-06-21 21:15:04 -0300

    Vista de facturas arreglada

--- Shopping_Buy_All/Shopping_Buy_All/Procesos/Facturacion_View.cs
+++ Shopping_Buy_All/Shopping_Buy_All/Procesos/Facturacion_View.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Shopping_Buy_All.ABMS.AccesoADatos;
 using Shopping_Buy_All.Entidades;
 using Shopping_Buy_All.Productos;
 
@@ -15,53 +16,17 @@ namespace Shopping_Buy_All
 {
     public partial class Facturacion_View : Form
     {
+        AD_Facturacion _datos = new AD_Facturacion();
         public Facturacion_View()
         {
             InitializeComponent();
-        }
-        private DataTable ObtenerFacturas(string nroFactura)
-        {
-            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBaseDatos"];
-            SqlConnection cn = new SqlConnection(cadenaConexion);
-            try
-            {
-                SqlCommand comand = new SqlCommand();
-                string consulta = "Select * From ComprasPorCliente";
-                comand.Parameters.Clear();
-                comand.Parameters.AddWithValue("@nroFactura", nroFactura);
-                comand.CommandType = CommandType.Text;
-                comand.CommandText = consulta;
-
-                cn.Open();
-                comand.Connection = cn;
-
-                DataTable tabla = new DataTable();
-
-                SqlDataAdapter da = new SqlDataAdapter(comand);
-                da.Fill(tabla);
-                return tabla;
-            }
-            catch (SqlException)
-            {
-                MessageBox.Show("No se pudo obtener facturas.\nError en la base de datos.", "ERROR");
-                throw;
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("No se pudo obtener facturas.\nError en la base de datos.", "ERROR");
-                throw;
-            }
-            finally
-            {
-                cn.Close();
-            }
         }
 
         private void buttonTarjeta_Click(object sender, EventArgs e)
         {
             if (textNroFactura.Text != "")
             {
-                tablaFacturas.DataSource = ObtenerFacturas(textNroFactura.Text);
+                tablaFacturas.DataSource = _datos._GetFacturaPorNro(textNroFactura.Text.ToString());
                 if (tablaFacturas.Rows.Count == 0)
                 {
                     MessageBox.Show("La factura no existe");
